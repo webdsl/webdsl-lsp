bb38b15599e60d73cdd9bee8447c4324095d5070
Ksawery Radziwiłowicz <ksawery.radziwilowicz@gmail.com>
2026-03-01 22:32:12 +0100

    add different port numbers

--- app/src/main/kotlin/org/webdsl/lsp/App.kt
+++ app/src/main/kotlin/org/webdsl/lsp/App.kt
@@ -3,10 +3,10 @@ package org.webdsl.lsp
 import org.eclipse.lsp4j.launch.LSPLauncher
 import java.net.ServerSocket
 
-fun main() {
-  val port = 1337
+fun main(args: Array<String>) {
+  val port = args.getOrNull(0)?.toIntOrNull() ?: 0
   val s = ServerSocket(port)
-  println("WebDSL language server launcher has been started and is listening on port $port!")
+  println("WebDSL language server launcher has been started and is listening on port ${s.localPort}!")
 
   while (true) {
     val client = s.accept()
